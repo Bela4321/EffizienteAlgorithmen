33526ac3193a1887595d8b6d2f81918461540b73
Steffen <50176440+Hochbotaniker@users.noreply.github.com>
2023-01-12 20:07:17 +0100

    Added Comments and javadoc methoc headers

--- src/MiniProjekt1/MaxFlowOpenAI.java
+++ src/MiniProjekt1/MaxFlowOpenAI.java
@@ -24,7 +24,10 @@ public class MaxFlowOpenAI {
     public int maxFlow = -1;
 
 
-    // Constructor
+    /**
+     * Constructor method
+     * @param numVertices : number of vertices to create
+     */
     public MaxFlowOpenAI(int numVertices) {
         this.numVertices = numVertices;
         this.adjacencyList = new ArrayList<>(numVertices);
@@ -33,7 +36,12 @@ public class MaxFlowOpenAI {
         }
     }
 
-    // Add an edge to the graph
+    /**
+     * Creates and adds an edge between two given vertices into the graph
+     * @param from : start vertex
+     * @param to : end vertex
+     * @param capacity : edge capacity
+     */
     public void addEdge(int from, int to, int capacity) {
         Edge forward = new Edge(from, to, capacity);
         Edge backward = new Edge(to, from, 0);
@@ -43,7 +51,12 @@ public class MaxFlowOpenAI {
         adjacencyList.get(to).add(backward);
     }
 
-    // Find the maximum flow from the source to the sink
+    /**
+     * Computing the maximum flow from given source to sink
+     * @param source : id of source vertex
+     * @param sink : id of sink vertex
+     * @return : integer value of maximum flow
+     */
     public int maximumFlow(int source, int sink) {
         int maxFlow = 0;
 
@@ -91,12 +104,21 @@ public class MaxFlowOpenAI {
         }
         return maxFlow;
     }
-    // Inner class representing an edge in the graph
+
+    /**
+     * Inner class representing an edge in the graph
+     */
     private static class Edge {
         int from, to;
         int capacity, flow;
         Edge backward; // The backward edge corresponding to this edge
 
+        /**
+         * Constructor method
+         * @param from : id of start vertex
+         * @param to : id of end vertex
+         * @param capacity : edge capacity
+         */
         public Edge(int from, int to, int capacity) {
             this.from = from;
             this.to = to;
@@ -116,7 +138,12 @@ public class MaxFlowOpenAI {
         }
     }
 
-    // Test the Ford Fulkerson algorithm
+    /**
+     * Main Method to create a MaxFlowOpenAI instance and generates a directed flow graph from a given file
+     * @param file : input file containing information about the graph
+     * @param ignoreL2 : boolean value if the vertex "L2" should be ignored (true) or not (false)
+     * @return : directed flow graph generated from file
+     */
     private static MaxFlowOpenAI maxFlow(File file, boolean ignoreL2) {
         //read csv file to table
         List<String[]> table = new ArrayList<>();
@@ -148,23 +175,27 @@ public class MaxFlowOpenAI {
         //add vertices and check capacity
         boolean first = true;
         for (String[] row : table) {
-            //skip first line
+            //skip first line since it contains only vertex names (headline of adjacency matrix)
             if (first) {
                 first = false;
                 continue;
             }
+            // check for information about vertex capacity (last column)
             String[] parts = row[row.length - 1].split(" ");
             if (parts[0].equals("Kapazitaet:")) {
-                    try{
-                        int capacityValue = Integer.parseInt(parts[1]);
-                        capacity.put(row[0], capacityValue);
-                        nameId.put(row[0], id);
-                        id = id + 2;
-                    } catch (Exception ignored){
-                        nameId.put(row[0], id);
-                        id++;
-                    }
+                try{
+                    // if the vertex has capacity, split it into two vertices with an edge between them containing the capacity value
+                    int capacityValue = Integer.parseInt(parts[1]);
+                    capacity.put(row[0], capacityValue);
+                    nameId.put(row[0], id);
+                    id = id + 2;
+                } catch (Exception ignored){
+                    // capacity value cannot be parsed -> ignore capacity and add only one vertex
+                    nameId.put(row[0], id);
+                    id++;
+                }
             } else {
+                // no capacity -> add only one vertex
                 nameId.put(row[0], id);
                 id++;
             }
@@ -179,6 +210,7 @@ public class MaxFlowOpenAI {
         }
         //other edges
         first = true;
+        // iterate adjacency (capacity) matrix
         for (String[] row : table) {
             //skip first line
             if (first) {
@@ -186,9 +218,9 @@ public class MaxFlowOpenAI {
                 continue;
             }
             for (int i = 1; i < row.length - 1; i++) {
-                //check if edge exists
+                // check if edge exists, 0 capacity -> skip
                 if (row[i].equals("0")) continue;
-                //add edge
+                // edge has capacity -> add to the graph
                 int start = nameId.get(row[0]);
                 if (capacity.containsKey(row[0])) {
                     start++;
@@ -211,6 +243,10 @@ public class MaxFlowOpenAI {
         return graph;
     }
 
+    /**
+     * A method that computes the optimal distribution between sources in order to minimize the number of cycles needed
+     * @param file : a file containing information about the graph
+     */
     public static void calcOptimalDistribution(File file){
         MaxFlowOpenAI graph = maxFlow(file, false);
         System.out.println("Optimale Lösung pro Durchgang:");
@@ -225,6 +261,7 @@ public class MaxFlowOpenAI {
         int neededCycles = (int) Math.ceil(1000./(double) graph.maxFlow);
         System.out.println("Benötigte Durchgänge: " + neededCycles);
         System.out.println("Optimale Lösung für alle Durchgänge:");
+        // calculating for 1000 presents
         int presentsLeft = 1000;
         for (String lager: new String[]{"L1","L2","L3","L4","L5"}){
             for (Edge edge : graph.adjacencyList.get(0)){
@@ -239,16 +276,23 @@ public class MaxFlowOpenAI {
         }
     }
 
+    /**
+     * A method that compares solutions of the maximum flow problem either including or ignoring the vertex "L2"
+     * and prints both solutions to the console.
+     * @param file : a file containing information about the graph
+     */
     public static void comparewithoutL2(File file){
+        // compute maxFlow for both cases (using and ignoring vertex L2)
         MaxFlowOpenAI graph1 = maxFlow(file, false);
         MaxFlowOpenAI graph2 = maxFlow(file, true);
-        //compare needed cycles
+
+        //compare needed cycles for both solutions
         int neededCycles1 = (int) Math.ceil(1000./(double) graph1.maxFlow);
         int neededCycles2 = (int) Math.ceil(1000./(double) graph2.maxFlow);
         if (neededCycles2>neededCycles1){
             System.out.println("Lösung ohne L2 ist schlechter, man benötigt " + (neededCycles2-neededCycles1) + " Durchgänge mehr");
         } else {
-            System.out.println("Lösung ohne L2 ist gleichgut, man benötigt immer noch " + neededCycles1 + " Durchgänge");
+            System.out.println("Lösung ohne L2 ist gleich gut, man benötigt immer noch " + neededCycles1 + " Durchgänge");
         }
         System.out.println("Optimale Lösung ohne L2:");
         int presentsLeft = 1000;
@@ -265,10 +309,18 @@ public class MaxFlowOpenAI {
         }
     }
 
+    /**
+     * A method that computes the maximum flow of a given graph and finds unnecessary edges that are not being used (edges with no flow)
+     * @param file : a file containing information about the graph
+     * @param ignoreL2 : boolean value if the vertex "L2" should be ignored (true) or not (false)
+     */
     public static void findUnusedPipes(File file, boolean ignoreL2){
 
+        // compute maxFlow
         MaxFlowOpenAI graph = MaxFlowOpenAI.maxFlow(file, ignoreL2);
         System.out.println("Unbenutzte Rohren:");
+
+        // iterate all edges and find ones with 0 flow that has >0 capacity and is not connected to source or sink
         for (int i = 0; i < graph.adjacencyList.size(); i++) {
             for (Edge edge : graph.adjacencyList.get(i)){
                 if (edge.flow == 0&&edge.capacity!=0&&edge.from!=0&&edge.to!=1){
@@ -279,18 +331,26 @@ public class MaxFlowOpenAI {
     }
 
 
-    //second for loop because of possible node splitting, when with node capacity
+    /**
+     * A method that locates a vertex name from the original file using a vertex id.
+     * This is necessary because some vertices have been split in two in order to implement vertex capacity
+     * @param id : vertex id in the graph
+     * @return : String that contains the original name
+     */
     private String reverseNameId(int id){
         for (String key : nameId.keySet()){
             if (nameId.get(key) == id){
                 return key;
             }
         }
+        // if name was not found in the hashmap, the vertex might have been split in two to implement vertex capacity
+        // in that case, the name is found at id-1
         for (String key : nameId.keySet()){
             if (nameId.get(key) == id-1){
                 return key;
             }
         }
+        // ... or there simply is no vertex with the given id
         return null;
     }
 }
